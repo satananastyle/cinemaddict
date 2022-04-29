f5dfddea403039020895ca6dbb48cc6f028c854c
satananastyle <scorpioshassol@outlook.com>
2022-04-29 12:46:09 +0300

    Добавляет отрисовку статистики в футер

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import RatingView from './view/rating-view.js';
 import FiltersView from './view/filters-view.js';
 import SortView from './view/sort-view.js';
 import BtnMoreView from './view/btn-more-view.js';
+import FooterStatisticsView from './view/footer-statistics-view.js';
 import { render } from './render.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
 
 render(new RatingView(), siteHeaderElement);
 render(new FiltersView(), siteMainElement);
 render(new SortView(), siteMainElement);
 render(new BtnMoreView(), siteMainElement);
+render(new FooterStatisticsView(), siteFooterStatisticsElement);
