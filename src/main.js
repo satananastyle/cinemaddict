e0a0adbe4709735b712c31425a09b1554942f162
satananastyle <scorpioshassol@outlook.com>
2022-04-29 12:28:41 +0300

    Добавляет отрисовку Фильтров

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import RatingView from './view/rating-view.js';
+import FiltersView from './view/filters-view.js';
 import { render } from './render.js';
 
 const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
 
 render(new RatingView(), siteHeaderElement);
+render(new FiltersView(), siteMainElement);
