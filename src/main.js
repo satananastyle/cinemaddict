f8b3afa685a4ccc12e3be1969be63945728ce18f
satananastyle <scorpioshassol@outlook.com>
2022-04-29 12:20:26 +0300

    Добавляет отрисовку рейтинга пользователя

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
+import RatingView from './view/rating-view.js';
+import { render } from './render.js';
 
+const siteHeaderElement = document.querySelector('.header');
+
+render(new RatingView(), siteHeaderElement);
